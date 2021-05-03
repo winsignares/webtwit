f9deff739a87ec100df862393ffcd1c3257f6b7b
= <=>
2021-05-03 15:23:41 -0500

    Cambio add

--- Backend/Personas/guardar.php
+++ Backend/Personas/guardar.php
@@ -1,4 +1,14 @@
 <?php
+    $nombre = "";
+    $apellidos = "";
+    $correo = "";
+    $password = "";
+    
+    $nombre = $_POST['nombre'];
+    $apellidos = $_POST['apellidos'];
+    $correo = $_POST['correo'];
+    $password = $_POST['password'];
 
-    echo "Hola estos son los datos de la persona";
+
+    echo "Hola estos son los datos de la persona ".$nombre;
 ?>
